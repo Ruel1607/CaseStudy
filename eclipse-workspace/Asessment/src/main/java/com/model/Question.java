3bf13d219330f0a96f53c97a2f99ff8959291ba8
RUEL IDOL V LACABA <Ruel.Lacaba@ibm.com>
2020-04-30 22:42:13 +0800

    UPDATE

--- eclipse-workspace/Asessment/src/main/java/com/model/Question.java
+++ eclipse-workspace/Asessment/src/main/java/com/model/Question.java
@@ -1,14 +1,13 @@
 package com.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Question {
 	
 	@Id
-	@GeneratedValue
+	
 	private Integer id; 
 	
 	private String subType; 
--- eclipse-workspace/Asessment/src/main/java/com/resource/QuestionResource.java
+++ eclipse-workspace/Asessment/src/main/java/com/resource/QuestionResource.java
@@ -1,14 +1,20 @@
 package com.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.model.Question;
 import com.repository.QuestionRepository;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,5 +47,44 @@ public class QuestionResource {
 		
 		return(List<Question>) qRepo.findByDifficultyLevel(diffLevel);
 	}
+	
+	@PutMapping("questions/{id}")
+	public ResponseEntity<Object> editQuestion(@RequestBody Question question,@PathVariable int id){
+		
+		
+		Optional<Question> questionOptional = qRepo.findById(id);
+		if (!questionOptional.isPresent())
+			return ResponseEntity.notFound().build();
+		
+		
+	question.setId(id);	
+	Question editQuestion = qRepo.save(question);
+		
+	URI location =	ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(editQuestion.getId()).toUri(); 
+	
+		return ResponseEntity.created(location).build();
+		
+	}
+	@PostMapping("questions")
+	public ResponseEntity<Object> createQuestion(@RequestBody Question question){
+		
+		
+		
+		
+		Question createQuestion = qRepo.save(question);
+		
+		URI location =	ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(createQuestion.getId()).toUri(); 
+		
+			return ResponseEntity.created(location).build();
+		
+	}
+	
+	
+	
+	
 	
 }
